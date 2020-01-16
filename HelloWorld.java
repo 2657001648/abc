f0690c3c5ba30ad70eb59aa22f8234d10aa3da12
2657001648 <=>
2020-01-16 17:28:41 +0800

    bbb

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("HelloWorld1");
+		System.out.println("999");
+		System.out.println("HelloWorld1");
+		System.out.println("888");
+		System.out.println("HelloWorld1");
+		System.out.println("777");
+
+	}
+}
